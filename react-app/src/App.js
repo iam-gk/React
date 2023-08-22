541d3a1836eb1571397110786c67320d0182d8bf
Gaurav <gauravkr6251@gmail.com>
2023-08-22 09:34:11 +0530

    Adding another function in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
+     <div className="App-header"> {/* css added */}
       Hello React
+      <Demo></Demo>
+      {/* Attach other functions by using tags inside parent function and 
+      declaring the new function after the parent function */}
+    </div>
+    </div>
+  );
+}
+
+function Demo() {
+  return (
+    <div className="App">
+      Demo
     </div>
   );
 }
