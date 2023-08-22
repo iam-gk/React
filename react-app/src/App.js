d8a2193d620ba7679cb319d85b707c6bc22deede
Gaurav <gauravkr6251@gmail.com>
2023-08-22 09:43:43 +0530

    Using variables inside return

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,24 @@
+import './App.css';
+
 function App() {
+
+  {/* Declaring variable  */}
+  let name = 'react app'  
   return (
     <div className="App">
      <div className="App-header"> {/* css added */}
       Hello React
+     <div className="App-header"> 
+      {name}      {/* It will return name = react app*/}
+      {/* Using variables inside return */}  
       <Demo></Demo>
       {/* Attach other functions by using tags inside parent function and 
       declaring the new function after the parent function */}
     </div>
     </div>
+    </div>
   );
 }
-
 function Demo() {
   return (
     <div className="App">
@@ -18,5 +26,4 @@ function Demo() {
     </div>
   );
 }
-
 export default App;
