02bb1e9f7d3b656ea2731ff819ee82cbb8e9f1ef
Gaurav <gauravkr6251@gmail.com>
2023-09-27 01:20:03 +0530

    15-> 3.Conditional rendering, maps (iii) Giving data in maps

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,61 @@
 import Video from "./components/Video";
 import "./App.css";
 function App() {
-  let obj = {
-     title:"Python JS tutorial" ,
-     views:"10k",
-    time:"1 year ago",
-    channel:"Coder Dost",
-    verified:true,
-  }
+  let videos = [
+    {
+      id: 1,
+      title: "Django JS tutorial",
+      views: "10k",
+      time: "1 year ago",
+      channel: "Coder Dost",
+      verified: true,
+    },
+    {
+      id: 2,
+      title: "Python JS tutorial",
+      views: "20k",
+      time: "2 year ago",
+      channel: "Coder Dost",
+      verified: false,
+    },
+    {
+      id: 3,
+      title: "Typescript JS tutorial",
+      views: "30k",
+      time: "3 year ago",
+      channel: "Coder Dost",
+      verified: true,
+    },
+    {
+      id: 4,
+      title: "React JS tutorial",
+      views: "40k",
+      time: "4 year ago",
+      channel: "Coder Dost",
+      verified: false,
+    },
+  ];
   return (
     <>
-    <div className="App">
-      <div>Videos</div>
-      <Video {...obj}></Video>
-      {/* here, for calling object(name obj) we used {...obj} spread operator. */}
-      <Video  verified={false}title="React JS tutorial" views="10k" time="1 year ago" channel="Coder Dost"></Video>
-      <Video  verified={false}title="Node Js tutoriail" views="100k" time="2 years ago" ></Video>
-      <Video  verified={true}title="Mongo Js tutoriail" views="1000k" time="3 years ago" channel="Coder Masti"></Video>
-      {/* Here, we have send title props to Video component. */}
+      <div className="App">
+        <div>Videos</div>
+        {videos.map((video) => (
+          <Video
+            verified={true}
+            key={video.id}
+            id={video.id}
+            title={video.title}
+            views={video.views}
+            time={video.time}
+            channel={video.channel}
+          ></Video>
+        ))}
+        {/* <Video {...videos}></Video> */}
+        {/* here, for calling object(name obj) we used {...obj} spread operator. */}
+        {/* <Video  verified={false}title="React JS tutorial" views="10k" time="1 year ago" channel="Coder Dost"></Video>
+      <Video  verified={false}title="Node Js tutoriail" views="100k" time="2 years ago" ></Video> */}
+        {/* <Video  verified={true}title="Mongo Js tutoriail" views="1000k" time="3 years ago" channel="Coder Masti"></Video>  */}
+        {/*  Here, we have send title props to Video component. */}
       </div>
     </>
   );
--- react-app/src/components/Video.js
+++ react-app/src/components/Video.js
@@ -1,9 +1,8 @@
 import "./Video.css";
 
-// function Video(props) 
-function Video({title,channel="gaurav",views,time,verified,})
-// Here, we set a default value channel="gaurav". gaurav is default value.
-{
+// function Video(props)
+function Video({ title, id, channel = "gaurav", views, time, verified }) {
+  // Here, we set a default value channel="gaurav". gaurav is default value.
   // let verified = true;
   // let channelJSX;
   // if(verified){
@@ -17,20 +16,22 @@ function Video({title,channel="gaurav",views,time,verified,})
   console.log("v", Video);
   return (
     <>
-    <div className="container">
-      <div className= "pic">
-        <img
-          src="https://picsum.photos/id/2/160/90"
-          alt="Background img"
-        />
+      <div className="container">
+        <div className="pic">
+          <img
+            src={`https://picsum.photos/id/${id}/160/90`}
+            alt="Background img"
+          />
+        </div>
+        <div className="title">{title}</div>
+        <div className="channel">
+          {channel} {verified ? "✅" : ""}{" "}
+        </div>
+        {/* {channelJSX} */}
+        <div className="views">
+          {views} <span>.</span> {time}
+        </div>
       </div>
-      <div className="title">{title}</div>
-       <div className="channel">{channel} {verified ? '✅': ''}  </div>
-      {/* {channelJSX} */}
-      <div className="views">
-        {views} <span>.</span> {time}
-        </div>
-        </div>
     </>
   );
 }
