81dc6ed7eb0b4eafa7c435fba8241414030f30d7
Gaurav <gauravkr6251@gmail.com>
2023-09-27 01:25:15 +0530

    16-> 3.Conditional rendering, maps (iv) Given data in new data file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,40 +1,8 @@
 import Video from "./components/Video";
 import "./App.css";
+import videos from './data/data';
 function App() {
-  let videos = [
-    {
-      id: 1,
-      title: "Django JS tutorial",
-      views: "10k",
-      time: "1 year ago",
-      channel: "Coder Dost",
-      verified: true,
-    },
-    {
-      id: 2,
-      title: "Python JS tutorial",
-      views: "20k",
-      time: "2 year ago",
-      channel: "Coder Dost",
-      verified: false,
-    },
-    {
-      id: 3,
-      title: "Typescript JS tutorial",
-      views: "30k",
-      time: "3 year ago",
-      channel: "Coder Dost",
-      verified: true,
-    },
-    {
-      id: 4,
-      title: "React JS tutorial",
-      views: "40k",
-      time: "4 year ago",
-      channel: "Coder Dost",
-      verified: false,
-    },
-  ];
+  
   return (
     <>
       <div className="App">
---
+++ react-app/src/data/data.js
@@ -0,0 +1,36 @@
+const videos = [
+    {
+      id: 1,
+      title: "Django JS tutorial",
+      views: "10k",
+      time: "1 year ago",
+      channel: "Coder Dost",
+      verified: true,
+    },
+    {
+      id: 2,
+      title: "Python JS tutorial",
+      views: "20k",
+      time: "2 year ago",
+      channel: "Coder Dost",
+      verified: false,
+    },
+    {
+      id: 3,
+      title: "Typescript JS tutorial",
+      views: "30k",
+      time: "3 year ago",
+      channel: "Coder Dost",
+      verified: true,
+    },
+    {
+      id: 4,
+      title: "React JS tutorial",
+      views: "40k",
+      time: "4 year ago",
+      channel: "Coder Dost",
+      verified: false,
+    },
+  ];
+
+  export default videos;
