14a1af1fa4ee7c61bd41c04f7fc93147c2eedbab
Gaurav <gauravkr6251@gmail.com>
2023-09-26 21:22:23 +0530

    12-> 2.Component, Props, JSX (vii) Calling data from object form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,18 @@
 import Video from "./components/Video";
 import "./App.css";
 function App() {
+  let obj = {
+     title:"Python JS tutorial" ,
+     views:"10k",
+    time:"1 year ago",
+    channel:"Coder Dost"
+  }
   return (
     <>
     <div className="App">
       <div>Videos</div>
+      <Video {...obj}></Video>
+      {/* here, for calling object(name obj) we used {...obj} spread operator. */}
       <Video  title="React JS tutorial" views="10k" time="1 year ago" channel="Coder Dost"></Video>
       <Video  title="Node Js tutoriail" views="100k" time="2 years ago" ></Video>
       <Video  title="Mongo Js tutoriail" views="1000k" time="3 years ago" channel="Coder Masti"></Video>
